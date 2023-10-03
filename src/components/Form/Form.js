842109ba9ed72c20fa07a663c046a1e2d27684f6
Joska <joska@MacBook-Air-de-Joska.local>
2023-10-03 16:00:06 +0200

    correct api

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -51,7 +51,7 @@ export default function Form() {
     useEffect(() => {
         if (steps === 5 && Object.keys(enteredContent).length === 4) {
             axios
-                .post('http://localhost:3001/api', enteredContent)
+                .post('https://gzefltel0a.execute-api.eu-west-3.amazonaws.com/app/form', enteredContent)
                 .then((res) => console.log(res))
                 .catch((err) => console.log(err));
         }
